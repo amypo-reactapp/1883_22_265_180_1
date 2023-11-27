c029577c02f75cd3bca5c1c870bd0207d3909759
amypo-reactapp <reactapp@amypo.in>
2023-11-27 10:27:57 +0000

    User

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from 'react';
+class TodoItem extends Component {
+ constructor(props) {
+ super(props);
+ this.state = {
+ completed: false
+ };
+ }
+ toggleCompleted = () => {
+ this.setState(prevState => ({
+ completed: !prevState.completed
+ }));
+ }
+ render() {
+ const { content } = this.props;
+ const { completed } = this.state;
+ return (
+ <li 
+ style={{ 
+ textDecoration: completed ? 'line-through' : 'none',
+ cursor: 'pointer' 
+ }}
+ onClick={this.toggleCompleted}
+ >
+ {content}
+ </li>
+ );
+ }
 }
-
-export default App;
+export default TodoItem;
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,13 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+import React from 'react';
+import TodoItem from './TodoItem'; // Adjust the import path as necessary
+function TodoList() {
+ const items = ["Item 1", "Item 2", "Item 3"]; // Example to-do items
+ return (
+ <ul>
+ {items.map((item, index) => (
+ <TodoItem key={index} content={item} />
+ ))}
+ </ul>
+ );
+}
+export default TodoList;
